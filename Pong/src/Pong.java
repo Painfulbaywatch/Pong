3a6d91501ade4ff6dd8634eaa258bec56ed49133
Painfulbaywatch <andrewwillis@myop.ac.nz>
2022-07-28 20:08:38 +1200

    created window created JPanel replaced "magic numbers" with static variables used invokeLater()

--- Pong/src/Pong.java
+++ Pong/src/Pong.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
 
 public class Pong extends JFrame {
@@ -6,17 +8,24 @@ public class Pong extends JFrame {
 	private final static int WINDOW_WIDTH = 800;
 	private final static int WINDOW_HEIGHT = 600;
 	
+	
+	
 	public Pong() {
 		setTitle(WINDOW_TITLE);
 		setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 		setResizable(false);
+		add(new PongPanel());
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new Pong();
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new Pong();
+			}
+		});
+		
 	}
 
 }
